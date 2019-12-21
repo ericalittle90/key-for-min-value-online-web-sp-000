fdb2de118804125fe2f6eaca4dc5f7c8f66e9b11
Erica Little <eteney90@gmail.com>
2019-12-21 17:46:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
   def key_for_min_value(name_hash)
     lowest = 0
     low_key = nil
-  
+
    name_hash.each do |key, value|
       if lowest == 0 || value < lowest
         lowest = value
@@ -12,6 +12,6 @@
       end
     end
     low_key
-  
+
   end
 end
